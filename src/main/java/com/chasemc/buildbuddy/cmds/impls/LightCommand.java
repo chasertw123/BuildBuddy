9849c7c955d4b628ee201cd1ecc6ce4f349cd8ee
Chase Henderson <henderson.chase@gmail.com>
2022-07-11 04:17:06 -0400

    The /light command now has the option of adding a number between 0 and 15. This will result in a light block that produces that light level when placed.

--- src/main/java/com/chasemc/buildbuddy/cmds/impls/LightCommand.java
+++ src/main/java/com/chasemc/buildbuddy/cmds/impls/LightCommand.java
@@ -3,8 +3,10 @@ package com.chasemc.buildbuddy.cmds.impls;
 import com.chasemc.buildbuddy.cmds.PlayerCommand;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.block.data.type.Light;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockDataMeta;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +19,35 @@ public class LightCommand extends PlayerCommand {
 
     @Override
     protected void onCommand(Player player, String[] args) {
-        player.getInventory().addItem(new ItemStack(Material.LIGHT));
-        player.sendMessage(ChatColor.GREEN + "Added a light block to your inventory!");
+        if (args.length > 1) {
+            player.sendMessage(ChatColor.RED + "Invalid Usage! /light [level]");
+            return;
+        }
+
+        int level = 15;
+        if (args.length == 1) {
+            try {
+                level = Integer.parseInt(args[0]);
+                if (level > 15 || level < 0) {
+                    throw new NumberFormatException("");
+                }
+            } catch (NumberFormatException e) {
+                player.sendMessage(ChatColor.RED + "Invalid light level of: " + ChatColor.GOLD + args[0]);
+                return;
+            }
+        }
+
+        ItemStack item = new ItemStack(Material.LIGHT);
+        BlockDataMeta meta = (BlockDataMeta) item.getItemMeta();
+
+        assert meta != null;
+        Light state = (Light) meta.getBlockData(Material.LIGHT);
+        state.setLevel(level);
+        meta.setBlockData(state);
+        item.setItemMeta(meta);
+
+        player.getInventory().addItem(item);
+        player.sendMessage(ChatColor.GREEN + "Added a level " + ChatColor.YELLOW + level + ChatColor.GREEN + " light block to your inventory!");
     }
 
     @Override
